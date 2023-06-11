fac551fd5fe090957b4f2fa6dab65a3929e2d05d
nazirmithu <nazirmithu22@gmail.com>
2023-06-11 16:52:40 +0600

    create jwt token with post method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +31,14 @@ async function run() {
         const allDataCollection = client.db("drawWiseDB").collection("allData");
         const usersCollection = client.db("drawWiseDB").collection("users");
         const cartCollection = client.db("drawWiseDB").collection("carts");
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({token})
+        })
+
 
         app.get('/popularclass', async (req, res) => {
             const result = await cartCollection.find().toArray();
